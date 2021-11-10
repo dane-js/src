e9997a3dc678a4bb4cd06b2d18477627782b0c53
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2021-11-10 18:21:05 +0100

    reogarnisation des models et plugins dans les controleurs

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -3,33 +3,47 @@ import { _http } from '../../types/_http';
 const Request = require('../http/Request');
 const Response = require('../http/Response');
 const fs = require('fs');
+const { in_array } = require('php-in-js/modules/array')
+const { ucfirst } = require('php-in-js/modules/string')
 module.exports = class BaseController 
 {
     protected path : {[key: string]: string};
 
-    protected resquest: _http.Request | null = null
+    protected request: _http.Request | null = null
     protected response: _http.Response | null = null
-    protected models : {[key: string]: _db.BaseModel} = {}
+    
+    protected db : {[key: string]: _db.BaseModel} = {}
 
     constructor(path: {[key: string]: string}) {
         this.path = path
     }
 
     public initialize(req : express.Request, res : express.Response, models : {[key: string]: _db.BaseModel}): void {
-        this.resquest = new Request(req)
-        this.response = new Response(res)
-        this.models = models
-
-        this.initPlugins()
+        this.request = new Request(req, res)
+        this.response = new Response(req, res)
+        
+        this.#initDb(models)
+        this.#initPlugins()
     }
 
-    private initPlugins(): void {
+    #initPlugins(): void {
         const pluginsConfig : {[key: string]: boolean | any} = require(this.path.CONFIG_DIR + '/plugins')
         for (let k in pluginsConfig) {
-            if (pluginsConfig[k] === true && fs.existsSync(this.path.PLUGIN_DIR + '/' + k)) {
-                const plugin = require(this.path.PLUGIN_DIR + '/' + k)
+            if (pluginsConfig[k] === true && fs.existsSync(`${this.path.PLUGIN_DIR}/${k}.js`)) {
+                const plugin = require(`${this.path.PLUGIN_DIR}/${k}.js`)
                 Object.defineProperties(this, {
                     ['$' + k]: { get: function() { return plugin } }
+                });
+            }
+        }
+    }
+
+    #initDb(models : {[key: string]: _db.BaseModel}): void {
+        this.db = models
+        for (let k in models) {
+            if (!in_array(k, ['sequelize', 'Sequelize', 'Op', 'DataTypes'])) {
+                Object.defineProperties(this, {
+                    [`${ucfirst(k)}Model`]: { get: function() { return models[k] } }
                 });
             }
         }
--- types/_controller.d.ts
+++ types/_controller.d.ts
@@ -1,7 +1,9 @@
 import { Request as ExpressRequest, Response as ExpressResponse } from 'express'
+import { _http } from './_http'
 declare namespace _controller {
     class BaseController {
-        protected response : _http.ServerResponse
+        protected request : _http.Request | null
+        protected response : _http.Response | null
         constructor(path: {[key: string]: string})
         public initialize(req : ExpressRequest, res : ExpressResponse, models : {[key: string]: _db.BaseModel})
     }
