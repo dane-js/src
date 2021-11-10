06406859fe343660c11ed75897112109da40b69f
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2021-11-10 18:18:13 +0100

    ajout du path des plugins

--- app/config/path.js
+++ app/config/path.js
@@ -1,6 +1,7 @@
 module.exports = {
+    CONFIG_DIR: `${__dirname}/../config`,
     CONTROLLER_DIR: `${__dirname}/../controllers`,
     MODEL_DIR: `${__dirname}/../models`,
-    STATIC_DIR: `${__dirname}/../static`,
-    CONFIG_DIR: `${__dirname}/../config`,
+    PLUGIN_DIR: `${__dirname}/../plugins`,
+    STATIC_DIR: `${__dirname}/../../static`,
 }
